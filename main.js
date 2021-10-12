cbdaf3d5a0de8c90cf9fef431f5fc84c4fb1112f
Tomek Sosniewski <sosna86@gmail.com>
2021-10-12 16:53:54 +0200

    next comm

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
 document.addEventListener('DOMContentLoaded', function (){
-    const rootElement = document.querySelector('#root');
-    const sections = document.querySelectorAll('section');
-    let currentSectionIndex = 0;
-    let isThrottled = false;
+    const scroller = new Scroller('#root')
+    console.log(scroller)
 
-    document.addEventListener('mousewheel', function (event) {
-        if(isThrottled)return;
-        isThrottled = true;
-        setTimeout(function(){
-            isThrottled = false
-        }, 1000)
-        const direction = event.wheelDelta < 0 ? 1 : -1;
-       scroll(direction)
+    document.addEventListener('mousewheel', scroller.listenScroll)
         
        
     })
-    function scroll(direction){
-        if(direction === 1){
-            const isLastSection = currentSectionIndex === sections.length -1; 
-            if(isLastSection)return;
-        }else if (direction === -1){
-            const firstSection = currentSectionIndex === 0;
-            if(firstSection)return;
-        }
+    // function scroll(direction){
+    //     if(direction === 1){
+    //         const isLastSection = currentSectionIndex === sections.length -1; 
+    //         if(isLastSection)return;
+    //     }else if (direction === -1){
+    //         const firstSection = currentSectionIndex === 0;
+    //         if(firstSection)return;
+    //     }
 
-        currentSectionIndex = currentSectionIndex + direction;
-        scrollToCurrentSection();
-    }
-    function scrollToCurrentSection() {
-         sections[currentSectionIndex].scrollIntoView({
-            behaviour: 'smooth',
-            block:'start',
-        })
-    }
-})
+    //     currentSectionIndex = currentSectionIndex + direction;
+    //     scrollToCurrentSection();
+    // }
+    // function scrollToCurrentSection() {
+    //      sections[currentSectionIndex].scrollIntoView({
+    //         behaviour: 'smooth',
+    //         block:'start',
+    //     })
+    // }
